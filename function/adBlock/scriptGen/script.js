a5eca0e354e8c9c5e2b660f0d62cdb8123ad2336
crysosancher <vibhupandey.vp@gmail.com>
2022-04-10 00:34:31 +0530

    list pushed

--- function/adBlock/scriptGen/script.js
+++ function/adBlock/scriptGen/script.js
@@ -1,16 +1,23 @@
-const fs = require('fs');
-let arr = [];
-const website = ["github.com","heroku.com","instagram.com"];
-for(let i=0;i<website.length;i++){
-    let web = website[i];
-    arr.push({
-        "id": i+1,
-        "priority": 1,
-        "action": { "type": "redirect", "redirect": { "url": "https://facebook.com" } },
-	"condition": {
-		"urlFilter": `://*.${web}/*`, "resourceTypes": ["main_frame"]
-	}
-    })
+const fs = require("fs");
+const arr = [];
+async function main() {
+  var text = fs.readFileSync("function/list/pornsite-list.txt", "utf8");
+  var textByLine = text.split("\n");
+  //console.log(textByLine.length);
+  for (let i = 0; i < textByLine.length; i++) {
+    let web = textByLine[i];
+    await arr.push({
+      id: i + 1,
+      priority: 1,
+      action: { type: "redirect", redirect: { url: "https://facebook.com" } },
+      condition: {
+        urlFilter: `://*.${web}/*`,
+        resourceTypes: ["main_frame"],
+      },
+    });
+  }
 }
-fs.writeFileSync('../rule_1.json', JSON.stringify(arr));
-console.log(arr);
+main().then(() => {
+  fs.writeFileSync("function/adBlock/rule_1.json", JSON.stringify(arr));
+  console.log(arr);
+});
