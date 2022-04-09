a4b591ad6a319caabe5765f5ed566e45eaf10626
Harsh4999 <trivediharsh49@gmail.com>
2022-04-10 02:34:21 +0530

    added landing pages

--- content.js
+++ content.js
@@ -1,3 +1,30 @@
 console.log("Your bacha is safe")
 console.log(location.href)
-//location.replace('http://example.com')
+let c=0;
+function randomUrl() {
+    let u = ['https://fightthenewdrug.org/wp-content/uploads/2021/05/FTND_YouAreNotABadPerson_v2-2021.jpg', 'http://powerline.io/', 'https://wealthygorilla.com/19-reasons-stop-watching-porn/', 'https://fightthenewdrug.org/40-reasons-you-should-quit-watching-porn-today/']
+    return u[generateRandom()];
+}
+function generateRandom(min = 0, max = 3) {
+    // find diff
+    let difference = max - min;
+    // generate random number 
+    let rand = Math.random();
+    // multiply with difference 
+    rand = Math.floor(rand * difference);
+
+    // add with min value 
+    rand = rand + min;
+
+    return rand;
+}
+if(location.href=="https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649535845/Bhai-Kya-Kar-Raha-Hai-Tu-Meme-Template-on-Ashneer-Grover_glgoqo.jpg"||
+    location.href=='https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536322/ias_zey7a3.jpg'||location.href=='https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536497/kaha_tz2les.jpg'
+    ||location.href=='https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536638/desh_yntxhl.jpg'||location.href=='https://res.cloudinary.com/ddgbz6b9y/image/upload/v1649538007/WhatsApp_Image_2022-04-10_at_2.16.03_AM_ncjwno.png'
+    ||location.href=='https://res.cloudinary.com/ddgbz6b9y/image/upload/v1649538007/WhatsApp_Image_2022-04-10_at_2.16.04_AM_1_yd46jj.png'){
+    let randmUrl = randomUrl();
+    console.log('here')
+    console.log(randmUrl)
+    location.replace(randmUrl);
+}
+
--- function/adBlock/scriptGen/script.js
+++ function/adBlock/scriptGen/script.js
@@ -20,7 +20,10 @@ async function main() {
   }
 }
 const randomUrl = ()=>{
-  let u = ['https://fightthenewdrug.org/wp-content/uploads/2021/05/FTND_YouAreNotABadPerson_v2-2021.jpg','http://powerline.io/','https://fightthenewdrug.org/things-to-do-instead-of-watch-porn/ ','https://wealthygorilla.com/19-reasons-stop-watching-porn/','https://fightthenewdrug.org/40-reasons-you-should-quit-watching-porn-today/']
+  let u = ['https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649535845/Bhai-Kya-Kar-Raha-Hai-Tu-Meme-Template-on-Ashneer-Grover_glgoqo.jpg','https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536322/ias_zey7a3.jpg',
+  'https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536497/kaha_tz2les.jpg','https://res.cloudinary.com/dy9tzgg9i/image/upload/v1649536638/desh_yntxhl.jpg',
+  'https://res.cloudinary.com/ddgbz6b9y/image/upload/v1649538007/WhatsApp_Image_2022-04-10_at_2.16.03_AM_ncjwno.png',
+  'https://res.cloudinary.com/ddgbz6b9y/image/upload/v1649538007/WhatsApp_Image_2022-04-10_at_2.16.04_AM_1_yd46jj.png']
   return u[generateRandom()];
 }
 function generateRandom(min = 0, max = 4) {
